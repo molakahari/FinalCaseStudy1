537f3ab8acdedd94f4bac9e1593ce008972cfe62
molakahari <44669398+molakahari@users.noreply.github.com>
2022-10-18 17:38:23 +0530

    Update FinalCaseStudy.java

--- src/main/java/com/awspractice/ccdemo/FinalCaseStudy.java
+++ src/main/java/com/awspractice/ccdemo/FinalCaseStudy.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FinalCaseStudy {
 
-	@GetMapping("")
-	public String sayHello() {
-		return "Hi i'm from final case study";
-	}
-	@GetMapping("/name")
-	public String add() {
-		return "My name is sayHello() created for final case stusy";
-	}
+	  @GetMapping("")
+    public String sayHello() {
+        return "Hi i'm from final case study production deployment testing on manual approval";
+    }
+    @GetMapping("/name")
+    public String add() {
+        return "My name is sayHello() created for final case stusy";
+    }
+    @GetMapping("/purpose")
+    public String hello() {
+        return "Testing & Demo purpose";
+    }
 }
